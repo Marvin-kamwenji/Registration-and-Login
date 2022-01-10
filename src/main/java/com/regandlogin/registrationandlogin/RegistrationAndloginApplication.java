d05570ab889bda278ded69897169a0116cbc9a53
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2022-01-10 13:08:00 +0300

    encrypting password and username

--- src/main/java/com/regandlogin/registrationandlogin/RegistrationAndloginApplication.java
+++ src/main/java/com/regandlogin/registrationandlogin/RegistrationAndloginApplication.java
@@ -1,9 +1,11 @@
 package com.regandlogin.registrationandlogin;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class RegistrationAndloginApplication {
 
     public static void main(String[] args) {
